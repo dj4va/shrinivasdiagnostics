dbc988b6bcd9dc438e095affa6e4cecb1f086282
puyup <hellopuyup@gmail.com>
2022-10-13 18:37:41 +0700

    improve homepage

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	const swiper = new Swiper('.swiper-hero', {
+		loop: true,
+	  
+		// If we need pagination
+		pagination: {
+		  el: '.swiper-pagination',
+		},
+	  
+		// Navigation arrows
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		},
+	});
+});
